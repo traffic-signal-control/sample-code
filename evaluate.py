af03c14e5c7b753ab27c46c946c08a3fed6c915c
xyh97 <xiongyh@zju.edu.cn>
2019-03-18 20:55:20 +0800

    modify evaluate and replace engine

--- evaluate.py
+++ evaluate.py
@@ -6,6 +6,14 @@ from sim_setting import sim_setting_control
 import argparse
 from utility import parse_arguments
 
+baseline_tt = {
+    "hangzhou_bc_tyc_1h_10_11_2021": 479.19,
+    "hangzhou_bc_tyc_1h_7_8_1848": 340.15,
+    "hangzhou_bc_tyc_1h_8_9_2231": 573.18,
+    "hangzhou_kn_hz_1h_7_8_827": 161.48,
+    "hangzhou_sb_sx_1h_7_8_1671": 258.61,
+}
+
 def main():
     args = parse_arguments()
     sim_setting = sim_setting_control
@@ -14,124 +22,46 @@ def main():
 
 
 def evaluate_one_traffic(dic_sim_setting, scenario):
-
     roadnetFile = "data/{}/roadnet.json".format(scenario)
     flowFile = "data/{}/flow.json".format(scenario)
     planFile = "data/{}/signal_plan.txt".format(scenario)
     outFile = "data/{}/evaluation.txt".format(scenario)
 
     if check(planFile, dic_sim_setting["num_step"]):
-        df_vehicle_actual_enter_leave = test_run(dic_sim_setting, roadnetFile, flowFile, planFile)
-        df_vehicle_planed_enter = get_planed_entering(flowFile, dic_sim_setting)
-        # add planed entering to actual leaving
-        tt = cal_travel_time(df_vehicle_actual_enter_leave, df_vehicle_planed_enter, outFile, dic_sim_setting)
+        tt = cal_travel_time(dic_sim_setting, roadnetFile, flowFile, planFile)
         print("====================== travel time ======================")
         print("scenario_{0}: {1:.2f} s".format(scenario, tt))
-        print("====================== travel time ======================")
+        print("====================== travel time ======================\n")
+        b = baseline_tt[scenario]
+        score = (b - tt)/b
+        print("====================== score ======================")
+        print("scenario_{0}: {1}".format(scenario, score))
+        print("====================== score ======================")
     else:
         print("planFile is invalid, Rejected!")
 
 
-def test_run(dic_sim_setting, roadnetFile, flowFile, planFile):
 
-
+def cal_travel_time(dic_sim_setting, roadnetFile, flowFile, planFile):
     eng = engine.Engine(dic_sim_setting["interval"], dic_sim_setting["threadNum"],
                         dic_sim_setting["saveReplay"], dic_sim_setting["rlTrafficLight"],
                         dic_sim_setting["changeLane"])
     eng.load_roadnet(roadnetFile)
     eng.load_flow(flowFile)
 
-    plan = open(planFile, "r")
-    plan.readline()
-
-    inter_name = "intersection_1_1"
-    list_all_lanes = list(eng.get_lane_vehicles().keys())
-    dic_roadnet = json.load(open(roadnetFile, "r"))
-    dic_intersections = {}
-    for inter in dic_roadnet["intersections"]:
-        dic_intersections[inter["id"]] = inter
-    list_exiting_lanes = []
-    list_non_exiting_lanes = []
-    for lane in list_all_lanes:
-        int_x, int_y, direc, l_id = list(map(int, lane.split('_')[1:]))
-        if direc == 0:
-            int_x_end = int_x + 1
-            int_y_end = int_y
-        elif direc == 1:
-            int_x_end = int_x
-            int_y_end = int_y + 1
-        elif direc == 2:
-            int_x_end = int_x - 1
-            int_y_end = int_y
-        elif direc == 3:
-            int_x_end = int_x
-            int_y_end = int_y - 1
-        if dic_intersections["intersection_{0}_{1}".format(int_x_end, int_y_end)]["virtual"]:
-            list_exiting_lanes.append(lane)
-        else:
-            list_non_exiting_lanes.append(lane)
-
-    # maintain list of vehicles in exiting lanes and non-exiting lanes
-    list_non_exiting_lane_vehicles_prev = []
-    dic_vehicle_enter_leave_time = {}
+    plan = pd.read_csv(planFile, sep="\t", header=0, dtype=int)
+    intersection_id = plan.columns[0]
 
     for step in range(dic_sim_setting["num_step"]):
-
-        current_time = eng.get_current_time()  # return a double, time past in seconds
-        lane_vehicles = eng.get_lane_vehicles()  # return a dict, {lane_id: [vehicle1_id, vehicle2_id, ...], ...}
-
-        list_non_exiting_lane_vehicles_cur = []
-        for lane in list_non_exiting_lanes:
-            list_non_exiting_lane_vehicles_cur += lane_vehicles[lane]
-        for vec in (set(list_non_exiting_lane_vehicles_cur) - set(list_non_exiting_lane_vehicles_prev)):
-            # new entering vehicles
-            dic_vehicle_enter_leave_time[vec] = {"enter_time": current_time, "leave_time": float("nan")}
-        for vec in (set(list_non_exiting_lane_vehicles_prev) - set(list_non_exiting_lane_vehicles_cur)):
-            # new left vehicles
-            dic_vehicle_enter_leave_time[vec]["leave_time"] = current_time
+        phase = int(plan.loc[step])
+        eng.set_tl_phase(intersection_id, phase)  
+        eng.next_step()
+        current_time = eng.get_current_time()                     
 
         if current_time % 100 == 0:
             print("Time: {} / {}".format(current_time, dic_sim_setting["num_step"]))
 
-        phase = int(plan.readline())
-        eng.set_tl_phase(inter_name, phase)  # set traffic light of intersection_1_1 to phase (phases of intersection is defined in roadnetFile)
-        eng.next_step()
-
-        list_non_exiting_lane_vehicles_prev = list_non_exiting_lane_vehicles_cur[:]
-
-    df_vehicle_enter_leave = pd.DataFrame(dic_vehicle_enter_leave_time).transpose()
-    assert np.all((df_vehicle_enter_leave["enter_time"] >= 0).values)
-    return df_vehicle_enter_leave
-
-
-def get_planed_entering(flowFile, dic_sim_setting):
-    list_flow = json.load(open(flowFile, "r"))
-    dic_traj = {}
-    for flow_id, flow in enumerate(list_flow):
-        list_ts_this_flow = []
-        for step in range(flow["startTime"], min(flow["endTime"] + 1, dic_sim_setting["num_step"])):
-            if step == flow["startTime"]:
-                list_ts_this_flow.append(step)
-            elif step - list_ts_this_flow[-1] >= flow["interval"]:
-                list_ts_this_flow.append(step)
-
-        for vec_id, ts in enumerate(list_ts_this_flow):
-            dic_traj["flow_{0}_{1}".format(flow_id, vec_id)] = {"planed_enter_time": ts}
-
-    return pd.DataFrame(dic_traj).transpose()
-
-
-def cal_travel_time(df_vehicle_actual_enter_leave, df_vehicle_planed_enter, outFile, dic_sim_setting):
-
-    df_res = pd.concat([df_vehicle_planed_enter, df_vehicle_actual_enter_leave], axis=1, sort=False)
-    assert len(df_res) == len(df_vehicle_planed_enter)
-
-    df_res["leave_time"].fillna(dic_sim_setting["num_step"], inplace=True)
-    df_res["travel_time"] = df_res["leave_time"] - df_res["planed_enter_time"]
-    travel_time = df_res["travel_time"].mean()
-    with open(outFile, "w") as f:
-        f.write("Travel time is %.2f seconds." % travel_time)
-    return travel_time
+    return eng.get_score()
 
 
 def check(planFile, num_step):
